da07ca5a30c5d962097673d8149f49d3ec09cf60
youanan <youanan@163.com>
2016-09-10 17:35:46 +0800

    4.Database

--- manage.py
+++ manage.py
@@ -3,8 +3,15 @@
 from flask_script import Manager, Server
 
 from app import app
+from app.models import Todo
 
 manager = Manager(app)
+
+@manager.command
+def save_todo():
+    todo = Todo(content = "My first todo at 1")
+    todo.save()
+
 
 manager.add_command("runserver",
         Server(host='0.0.0.0',port=5000,use_debugger=True)
