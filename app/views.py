4cd8d69ab3740929f1476b58747621ab65fdd0b8
youanan <youanan@163.com>
2016-09-11 10:02:43 +0800

    update todo

--- app/views.py
+++ app/views.py
@@ -1,18 +1,36 @@
 from app import app
-from flask import render_template,request
+from flask import render_template, request
 from app.models import Todo
+
 
 @app.route('/')
 def index():
     todos = Todo.objects.all()
-    return render_template("index.html",todos=todos)
+    return render_template("index.html", todos=todos)
 
-@app.route("/add",methods=["POST",])
+
+@app.route("/add", methods=["POST", ])
 def add():
     content = request.form.get('content')
     todo = Todo(content=content)
     todo.save()
     todos = Todo.objects.all()
-    return  render_template("index.html",todos=todos)
+    return render_template("index.html", todos=todos)
 
 
+@app.route('/done/<string:todo_id>')
+def done(todo_id):
+    todo = Todo.objects.get_or_404(id=todo_id)
+    todo.status = 1
+    todo.save()
+    todos = Todo.objects.all()
+    return render_template("index.html",todos=todos)
+
+
+@app.route('/undone/<string:todo_id>')
+def undone(todo_id):
+    todo = Todo.objects.get_or_404(id=todo_id)
+    todo.status = 0
+    todo.save()
+    todos = Todo.objects.all()
+    return render_template("index.html",todos=todos)
--- manage.py
+++ manage.py
@@ -7,28 +7,16 @@ from app.models import Todo
 
 manager = Manager(app)
 
+
 @manager.command
 def save_todo():
-    todo = Todo(content = "todolist-0001")
+    todo = Todo(content="todolist-0001")
     todo.save()
 
 
 manager.add_command("runserver",
-        Server(host='0.0.0.0',port=5000,use_debugger=True)
-        )
+                    Server(host='0.0.0.0', port=5000, use_debugger=True)
+                    )
 
 if __name__ == "__main__":
     manager.run()
-
-
-
-
-
-
-
-
-
-
-
-
-
